41b2bb24368bee5f9ce1d1207247b13462093f92
Dogu <dogudeniz.ugur@gmail.com>
2020-07-26 23:55:23 +0300

    Minor design changes names file added

--- chalicelib/image_generation.py
+++ chalicelib/image_generation.py
@@ -1,6 +1,4 @@
-
 from PIL import Image, ImageFilter, ImageDraw, ImageFont
-import sys
 
 base = Image.open('base.jpg').convert('RGBA')
 
@@ -10,14 +8,27 @@ im = Image.new('RGBA', base.size, (255,255,255,0))
 # get a drawing context
 draw = ImageDraw.Draw(im)
 main_color = '#ff0000'
-name = 'Dogu'
+name = 'Do'+u'\u011f'+'u'
+
+# Draw the squares
 draw.rectangle(((0, 00), (750, 750)), main_color)
 draw.rectangle(((125, 25), (625, 725)), '#ffffff')
-draw.rectangle(((130, 30), (600, 500)), main_color)
+draw.rectangle(((135, 35), (615, 520)), main_color)
 
-fnt = ImageFont.truetype('Roboto-Medium.ttf', 60)
-draw.text((140, 520), 'Filsiz Boya'+u'\u00ae', 'black', font=fnt)
-draw.text((140, 620), name, 'black', font=fnt)
+# Texts
+# Brand Text
+brand_fnt = ImageFont.truetype('Roboto-Medium.ttf', 70)
+brand = 'Filsiz Boya'
+draw.text((140, 520), brand, 'black', font=brand_fnt)
+
+# Registered Symbol
+brand_length = brand_fnt.getsize(brand)
+registered_fnt = ImageFont.truetype('Roboto-Medium.ttf', 30)
+draw.text((140+brand_length[0], 530), u'\u00ae', 'black', font=registered_fnt)
+
+# Name and color
+name_fnt = ImageFont.truetype('Roboto-Medium.ttf', 40)
+draw.text((140, 520+brand_length[1]), name, 'black', font=name_fnt)
 
 
 '''
---
+++ chalicelib/names/README.md
@@ -0,0 +1,4 @@
+# Türkçe İsimler
+Türkçe erkek/kadın isimleri ve görülme sayıları (2009).
+
+Excel'de açtığınızda türkçe karakterleri göremiyorsanız [burayı okuyun](http://stackoverflow.com/a/6488070).
